e59d0abe99c175091368528b0aaaf3529932c30c
Titus Wormer <tituswormer@gmail.com>
2015-06-03 13:50:53 +0200

    Fix example

--- example.js
+++ example.js
@@ -2,7 +2,7 @@
 var toc = require('./index.js');
 var fs = require('fs');
 var mdast = require('mdast').use(toc);
-var readme = fs.readFileSync('Readme.md', 'utf-8');
+var readme = fs.readFileSync('readme.md', 'utf-8');
 
 // Parse markdown (this TOC is the 14th child).
 var contents = mdast.run(mdast.parse(readme)).children[14];
--- readme.md
+++ readme.md
@@ -57,7 +57,7 @@ Require/read dependencies:
 var toc = require('mdast-toc');
 var fs = require('fs');
 var mdast = require('mdast').use(toc);
-var readme = fs.readFileSync('Readme.md', 'utf-8');
+var readme = fs.readFileSync('readme.md', 'utf-8');
 ```
 
 Parse markdown (this TOC is the 14th child).
