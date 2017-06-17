0cf9c722ba1bc0a6344b7cdffca0f4d67563728a
Titus Wormer <tituswormer@gmail.com>
2017-06-17 14:56:01 +0200

    Add skipped test again

--- test/index.js
+++ test/index.js
@@ -36,15 +36,12 @@ test('Fixtures', function (t) {
     var output = read(join(filepath, 'output.md'), 'utf-8');
     var input = read(join(filepath, 'input.md'), 'utf-8');
     var config = join(filepath, 'config.json');
-    var fn;
     var result;
 
     config = exists(config) ? JSON.parse(read(config, 'utf-8')) : {};
     result = process(input, config);
 
-    fn = path.basename(filepath) === 'unicode' ? t.skip : t.equal;
-
-    fn(result, output, 'should work on `' + fixture + '`');
+    t.equal(result, output, 'should work on `' + fixture + '`');
   });
 
   t.end();
