bc56b81277fb7141a583edf24d2438ac9c15e544
Jonathan Haines <jonno.haines@gmail.com>
2020-01-18 09:13:56 +1100

    Pick TOCSettings from TOCOptions

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,36 +6,7 @@ import {Heading} from 'mdast'
 import {TOCOptions} from 'mdast-util-toc'
 
 declare namespace remarkToc {
-  interface TOCSettings {
-    /**
-     * Heading to look for, wrapped in `new RegExp('^(' + value + ')$', 'i')`.
-     *
-     * @default `'toc|table[ -]of[ -]contents?'`
-     */
-    heading?: TOCOptions['heading']
-
-    /**
-     * Maximum heading depth to include in the table of contents,
-     * This is inclusive, thus, when set to `3`, level three headings,
-     * are included (those with three hashes, `###`).
-     *
-     * @default 6
-     */
-    maxDepth?: TOCOptions['maxDepth']
-
-    /**
-     * Whether to compile list-items tightly.
-     *
-     * @default false
-     */
-    tight?: TOCOptions['tight']
-
-    /**
-     *  Headings to skip, wrapped in `new RegExp('^(' + value + ')$', 'i')`.
-     *  Any heading matching this expression will not be present in the table of contents.
-     */
-    skip?: TOCOptions['skip']
-  }
+  type TOCSettings = Pick<TOCOptions, 'heading' | 'maxDepth' | 'tight' | 'skip'>;
 }
 
 declare function remarkToc(
