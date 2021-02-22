1d58a519c78a5ae7b26878e165de50f3d10381e8
Titus Wormer <tituswormer@gmail.com>
2021-02-22 12:45:26 +0100

    Update dev-dependencies

--- test/index.js
+++ test/index.js
@@ -4,53 +4,41 @@ var fs = require('fs')
 var path = require('path')
 var test = require('tape')
 var remark = require('remark')
-var negate = require('negate')
 var hidden = require('is-hidden')
 var toc = require('..')
 
-var read = fs.readFileSync
-var join = path.join
-
-var root = join(__dirname, 'fixtures')
-
-var fixtures = fs.readdirSync(root)
-
-function process(value, config) {
-  return remark()
-    .use({settings: {bullet: '-'}})
-    .use(toc, config)
-    .processSync(value)
-    .toString()
-}
-
-test('remark-toc()', function (t) {
-  t.equal(typeof toc, 'function', 'should be a function')
-
-  t.doesNotThrow(function () {
-    toc.call(remark())
-  }, 'should not throw if not passed options')
-
-  t.end()
-})
-
 test('Fixtures', function (t) {
-  fixtures.filter(negate(hidden)).forEach(function (fixture) {
-    var filepath = join(root, fixture)
-    var output = read(join(filepath, 'output.md'), 'utf-8')
-    var input = read(join(filepath, 'input.md'), 'utf-8')
-    var config
-    var result
+  var root = path.join(__dirname, 'fixtures')
+  var fixtures = fs.readdirSync(root)
+  var index = -1
+  var fixture
+  var config
+
+  while (++index < fixtures.length) {
+    fixture = fixtures[index]
+
+    if (hidden(fixture)) {
+      continue
+    }
 
     try {
-      config = JSON.parse(read(join(filepath, 'config.json')))
+      config = JSON.parse(
+        fs.readFileSync(path.join(root, fixture, 'config.json'))
+      )
     } catch (_) {
       config = {}
     }
 
-    result = process(input, config)
-
-    t.equal(result, output, 'should work on `' + fixture + '`')
-  })
+    t.equal(
+      remark()
+        .use({settings: {bullet: '-'}})
+        .use(toc, config)
+        .processSync(fs.readFileSync(path.join(root, fixture, 'input.md')))
+        .toString(),
+      String(fs.readFileSync(path.join(root, fixture, 'output.md'))),
+      'should work on `' + fixture + '`'
+    )
+  }
 
   t.end()
 })
