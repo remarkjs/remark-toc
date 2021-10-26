1bcf3703cb35639d268c8fb0f1d9258efa764789
Titus Wormer <tituswormer@gmail.com>
2021-10-26 15:16:02 +0200

    Update dev-dependencies

--- test/index.js
+++ test/index.js
@@ -2,8 +2,8 @@
  * @typedef {import('../index.js').Options} Options
  */
 
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import test from 'tape'
 import {remark} from 'remark'
 import {isHidden} from 'is-hidden'
